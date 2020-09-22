8ebd819ce66b8aeb06ef836d5ef8310c46015a49
Tohny123 <jennycui826@gmail.com>
2020-09-22 14:50:19 -0400

    Lock Level (Broken)
    - Added Level Lock
    - Added Level Win Feature (Broken)

---
+++ Game3/Screen Boy/Assets/Scripts/LevelWin.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelWin : MonoBehaviour
+{
+    public string nextlevel;
+    public int unlockedlevel;
+    
+    private void OnTriggerEnter(Collider other)
+    {
+       
+        if(other.gameObject.tag == "Player")
+        {
+            if (PlayerPrefs.GetInt("levelReached") < unlockedlevel)
+            {
+                PlayerPrefs.SetInt("levelReached", unlockedlevel);   
+                SceneManager.LoadScene("Level Select");
+            }
+        }
+        
+
+    }
+}
