21e57e1d22f141415eabbc1e167e58ce2f9a4e8c
Tohny123 <jennycui826@gmail.com>
2020-10-09 19:03:51 -0400

    Enemy Tweak
    -Enemy how has a detection range

--- Game3/Screen Boy/Assets/Scripts/Enemy/Enemy.cs
+++ Game3/Screen Boy/Assets/Scripts/Enemy/Enemy.cs
@@ -7,17 +7,25 @@ public class Enemy : MonoBehaviour
     public GameObject plr;
     public NavMeshAgent agent;
     private Vector3 plrlocation;
+    public float radius;
+    public Animator anim;
     // Start is called before the first frame update
     void Start()
     {
         plr = GameObject.Find("Player");
-        plrlocation = plr.transform.position;
+    
     }
 
     // Update is called once per frame
     void Update()
     {
         plrlocation = plr.transform.position;
-        agent.SetDestination(plrlocation);
+        float distance = Vector3.Distance(plrlocation, transform.position);
+        anim.SetFloat("Speed", (Mathf.Abs(agent.velocity.x)) + (Mathf.Abs(agent.velocity.y)));
+        if(distance <= radius)
+        {
+            agent.SetDestination(plrlocation);
+        }
+        
     }
 }
