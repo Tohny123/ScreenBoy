f1bc90271701024747cc39817c3a4dbde04c5843
Tohny123 <jennycui826@gmail.com>
2020-10-11 13:04:20 -0400

    Unfinished Dialogue System
    -Added Unfinished Dalogue system

---
+++ Game3/Screen Boy/Assets/Scripts/Dialogue/DialogueManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueManager : MonoBehaviour
+{
+
+    public Queue<string> sentences;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        sentences = new Queue<string>();
+
+    }
+
+    public void dialoguestart (Dialogue dialogue) 
+    {
+        Debug.Log(dialogue.name);
+        sentences.Clear();
+        foreach (string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+        displaysentence();
+    }
+   
+   public void displaysentence()
+   {
+       if (sentences.Count == 0)
+       {
+           enddialogue();
+           return;
+       }
+       string sentence = sentences.Dequeue();
+       Debug.Log(sentence);
+   }
+    void enddialogue()
+    {
+        Debug.Log("end");
+    }
+}
---
+++ Game3/Screen Boy/Assets/Scripts/Dialogue/DialogueTrigger.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour
+{
+    public Dialogue dialogue;
+    public GameObject cameraref;
+    public void triggerdialogue ()
+    {
+        FindObjectOfType<DialogueManager>().dialoguestart(dialogue);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Time.timeScale = 0f;
+            triggerdialogue();
+            Cursor.lockState = CursorLockMode.None;
+            cameraref.GetComponent<Camera>().enabled = false;
+        }
+    }
+}
