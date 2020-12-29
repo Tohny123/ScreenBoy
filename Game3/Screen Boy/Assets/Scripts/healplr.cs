413979dbf1b9589186c05aa60ae44fb89a2b1294
Tohny123 <jennycui826@gmail.com>
2020-12-29 14:43:01 -0500

    Health and levels
    -added health
    -finished level 4

---
+++ Game3/Screen Boy/Assets/Scripts/healplr.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class healplr : MonoBehaviour
+{
+    public GameObject gameman;
+    public Health health;
+    public int healby;
+    public GameObject parent;
+    public GameObject particles;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameman = GameObject.Find("GameManager");
+        health = gameman.GetComponent<Health>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            health.heal(healby);
+            Destroy(parent);
+            Instantiate(particles, transform.position, transform.rotation);
+        }
+    }
+}
