d57675dba7ebec12a5d46207f39d6db337deaed5
Tohny123 <jennycui826@gmail.com>
2021-01-02 13:31:02 -0500

    easter egg update
    - added easter egg for all levels
    - finished level 9 and 10

---
+++ Game3/Screen Boy/Assets/Scripts/easteregg.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class easteregg : MonoBehaviour
+{
+    public string lvl;
+    private void OnTriggerEnter(Collider other)
+    {
+        //when player enters deal damage
+        if(other.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene(lvl);
+        }
+    }
+}
