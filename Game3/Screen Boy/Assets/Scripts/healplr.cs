150c6971e64dff1c9f0beaf4e1c1e92fc5228d7e
Tohny123 <jennycui826@gmail.com>
2021-02-12 12:23:55 -0500

    added sound for heal
    -added sound for heal
    -added health on final stage

--- Game3/Screen Boy/Assets/Scripts/healplr.cs
+++ Game3/Screen Boy/Assets/Scripts/healplr.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class healplr : MonoBehaviour
 {
@@ -9,17 +10,24 @@ public class healplr : MonoBehaviour
     public int healby;
     public GameObject parent;
     public GameObject particles;
+    public AudioClip healsound;
+    public AudioMixer audmix;
+    public float tempvol; 
+    public float healvol;   
     // Start is called before the first frame update
     void Start()
     {
         gameman = GameObject.Find("GameManager");
         health = gameman.GetComponent<Health>();
+        audmix.GetFloat("Volume", out tempvol);
+        healvol = 1/Mathf.Abs(tempvol);        
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player")
         {
+            AudioSource.PlayClipAtPoint(healsound, transform.position, healvol);
             health.heal(healby);
             Destroy(parent);
             Instantiate(particles, transform.position, transform.rotation);
