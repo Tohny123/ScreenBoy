2923b72798717decfc82243d10525ecd210d8188
Tohny123 <jennycui826@gmail.com>
2021-05-18 09:48:34 -0400

    Fixed level loading
    Fixed level loading

---
+++ Game3/Screen Boy/Assets/Scripts/Menu/LevelLoad.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelLoad : MonoBehaviour
+{
+    public Fade fade;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        fade = GameObject.Find("Fade").GetComponent<Fade>();
+    }
+
+   public void load(string LevelName)
+    {
+        StartCoroutine(fade.LevelFade(LevelName));
+    }
+}
